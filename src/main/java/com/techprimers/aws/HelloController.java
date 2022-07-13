d4e1a0f09778f057709346ac26175a2568d123fc
sathishreddy844 <105142374+sathishreddy844@users.noreply.github.com>
2022-07-13 15:11:42 +0530

    change pipeline1

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "new change on repo for code pipeline";
+        return "new change pipeline1 on repo for code pipeline";
     }
 }
